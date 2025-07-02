ec2a6d4f8ff39c530556d4da6aee46660f97f69e
Kydens <nathsyahed@gmail.com>
2025-07-03 02:56:48 +0700

    Add: tailwindcss

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,11 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   srcDir: 'src/',
-  // plugins: ['~/plugins/fetch-interceptor.js'],
   runtimeConfig: {
     public: {
       apiBase: process.env.URL_API,
     },
+  },
+  css: ['~/assets/css/main.css'],
+  plugins: [
+    { src: '~/plugins/fetch-interceptor.js', mode: 'client' },
+    { src: '~/plugins/swal.js', mode: 'client' },
+  ],
+  vite: {
+    plugins: [tailwindcss()],
   },
   compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
