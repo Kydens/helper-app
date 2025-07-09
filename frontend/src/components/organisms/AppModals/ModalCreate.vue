422d6b1002c51f1b65aa25490c2a0868c6b03201
Kydens <nathsyahed@gmail.com>
2025-07-10 01:27:39 +0700

    Add: Create Modal Modular

---
+++ frontend/src/components/organisms/AppModals/ModalCreate.vue
@@ -0,0 +1,80 @@
+<template>
+  <Form
+    v-slot="{ handleSubmit }"
+    as=""
+    keep-values
+    :validation-schema="formSchema"
+  >
+    <Dialog
+      :open="modalValue"
+      @update:open="(v) => emit('update:modalValue', v)"
+    >
+      <DialogContent class="sm:max-w-[425px]">
+        <DialogHeader class="border-b-2 border-seconday pb-2">
+          <DialogTitle class="font-bold">Tambah {{ title }}</DialogTitle>
+        </DialogHeader>
+
+        <form id="submitForm" class="flex flex-col gap-4 px-2 overflow-y-auto">
+          <slot />
+        </form>
+
+        <DialogFooter>
+          <DialogClose as-child>
+            <Button
+              type="button"
+              variant="outline"
+              class="transition-all hover:cursor-pointer"
+            >
+              Close
+            </Button>
+          </DialogClose>
+          <Button
+            type="submit"
+            @click="handleSubmit(handleSubmitForm)"
+            class="transition-all hover:cursor-pointer"
+          >
+            Create
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  </Form>
+</template>
+
+<script setup>
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogClose,
+} from '@/components/ui/dialog';
+import { Form } from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+
+const props = defineProps({
+  modalValue: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  title: {
+    type: String,
+    required: true,
+    default: '',
+  },
+  formSchema: {
+    type: Object,
+    required: true,
+  },
+});
+
+const emit = defineEmits(['update:modalValue', 'submit']);
+
+const handleSubmitForm = (values) => {
+  emit('submit', values);
+  emit('update:modalValue', false);
+};
+</script>
+
+<style></style>
